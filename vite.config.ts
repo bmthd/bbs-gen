f9194353e9a2170d8dc71059fd30fb36ad17e419
bmthd <jougennotuki67@gmail.com>
2024-05-12 01:34:53 +0900

    update:path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,37 @@
+/// <reference types="vitest"/>
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import { defaultConfig, getColorModeScript } from "@yamada-ui/react"
+import * as path from 'path'
+import { Plugin, defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
+const injectScript = (): Plugin => {
+  return {
+    name: "vite-plugin-inject-scripts",
+    transformIndexHtml(html) {
+      const content = getColorModeScript({
+        initialColorMode: defaultConfig.initialColorMode,
+      })
+      return html.replace("<body>", `<body><script>${content}</script>`)
+    },
+  }
+}
+
 export default defineConfig({
-  plugins: [react(),TanStackRouterVite(),],
+  plugins: [react(),TanStackRouterVite(),injectScript()],
+  resolve: {
+    alias: [
+        { find: '@', replacement: path.resolve(__dirname, 'src') },
+    ],
+  },
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    setupFiles: "./src/test/setup.ts",
+    coverage: {
+      enabled: !process.env.CI,
+      reportsDirectory: "coverage",
+    },
+    exclude: ["./node_modules/**", "./e2e/**/*"],
+  },
 })
